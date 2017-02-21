5a43f567079281591897a38fea8cb53a566c7df6
anseee <admin@ansee.kr>
2017-02-21 16:47:37 +0900

    Create button and change button shape of circle

--- AnimationWhenTouchCircleView/AnimationWhenTouchCircleView/ViewController.swift
+++ AnimationWhenTouchCircleView/AnimationWhenTouchCircleView/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var circleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.circleButton.layer.masksToBounds = true
+        self.circleButton.layer.cornerRadius = self.circleButton.frame.size.width / 2
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func touchButton(_ sender: Any) {
+        
+    }
+    
 }
 
